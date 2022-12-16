6a4179a970bbfd60a88203c789b956f881362310
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-12-16 11:36:38 +0800

    conversion to json format

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 // #include <s8_uart.h>
 #include <SCD30.h>
 
+#include <ArduinoJson.h>
+
 #define RED_LED 13
 #define YELLOW_LED 12
 #define GREEN_LED 11
@@ -34,17 +36,20 @@ int pir_state = LOW;
 int sensing_state = 0;
 
 unsigned long time;
-
 float result[3] = {0};
-
+float heatIndex = 0;
+StaticJsonDocument<64> doc;
 
 void configureSensor(void);
 
 void setup() {
+
+    
+
     Wire.begin();
     // Configure serial port, we need it for debug
-    Serial.begin(9600);
-    //Serial.begin(115200);
+
+    Serial.begin(115200);
 
 
     // others 
@@ -59,9 +64,9 @@ void setup() {
 
     // light sensor
     if (tsl.begin()) {
-        Serial.println(F("Found a TSL2591 sensor"));
+        //Serial.println(F("Found a TSL2591 sensor"));
     } else {
-        Serial.println(F("No sensor found ... check your wiring?"));
+        //Serial.println(F("No sensor found ... check your wiring?"));
         while (1);
     }
     configureSensor();
@@ -114,39 +119,49 @@ void loop() {
         if (pir_state == HIGH) pir_state = LOW;
     }
 
-    if ( millis() - 100 > time) {
+    if ( millis() - 1000 > time) {
 
-        Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] ")); 
-        if (pir_state == HIGH){
-            Serial.print("motion   @ "); Serial.println("true");
-            //Serial.println("Motion detected!");	// print on output change
-        }
-        if (pir_state == LOW){
-            Serial.print("motion   @ "); Serial.println("false");
-            //Serial.println("No Motion");	// print on output change
-        } 
+        // Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] ")); 
+        // if (pir_state == HIGH){
+        //     Serial.print("motion   @ "); Serial.println("true");
+        //     //Serial.println("Motion detected!");	// print on output change
+        // }
+        // if (pir_state == LOW){
+        //     Serial.print("motion   @ "); Serial.println("false");
+        //     //Serial.println("No Motion");	// print on output change
+        // } 
+
+        doc["Motion"] = pir_state;
         
         if (scd30.isAvailable()) {
             scd30.getCarbonDioxideConcentration(result);
 
-            Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
-            Serial.print("c02      @ "); Serial.print(result[0]); Serial.println(" ppm");
+            // Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
+            // Serial.print("c02      @ "); Serial.print(result[0]); Serial.println(" ppm");
 
-            Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] ")); 
-            Serial.print("temp     @ "); Serial.print(result[1]); Serial.println(" celsius");
+            // Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] ")); 
+            // Serial.print("temp     @ "); Serial.print(result[1]); Serial.println(" celsius");
             
-            Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
-            Serial.print("humid    @ "); Serial.print(result[2]); Serial.println(" percent"); 
+            // Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
+            // Serial.print("humid    @ "); Serial.print(result[2]); Serial.println(" percent"); 
 
-            Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
-            Serial.print("heat     @ "); Serial.print(dht.computeHeatIndex(result[1],result[2])); Serial.println(" celsius");
+            heatIndex = dht.computeHeatIndex(result[1],result[2]);
+
+            // Serial.print(F("[ ")); Serial.print(millis());Serial.print(F(" ms ] "));
+            // Serial.print("heat     @ "); Serial.print(heatIndex); Serial.println(" celsius");
+
+            
+            doc["HeatIndex"] = heatIndex ;
+            doc["C02"] = result[0];
+            doc["Temperature"] = result[1];
+            doc["Humidity"] = result[2];
         }
 
         
         sensors_event_t event;
 
         tsl.getEvent(&event);
-        Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
+        //Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
         if ((event.light == 0) |
             (event.light > 4294966000.0) | 
             (event.light <-4294966000.0))
@@ -158,14 +173,18 @@ void loop() {
         }
         else
         {
-            Serial.print("light    @ ");Serial.print(event.light); Serial.println(F(" lux"));
+            //Serial.print("light    @ ");Serial.print(event.light); Serial.println(F(" lux"));
+            doc["Light"] = event.light;
         } 
 
-
-
-
         time = millis();
+        doc["Time"] = time;
+        serializeJson(doc, Serial);  
+        // Serial.println("");
     }
+
+
+
 
 }
 
@@ -186,27 +205,27 @@ void configureSensor(void)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
   /* Display the gain and integration time for reference sake */  
-  Serial.println(F("------------------------------------"));
-  Serial.print  (F("Gain:         "));
+//   Serial.println(F("------------------------------------"));
+//   Serial.print  (F("Gain:         "));
   tsl2591Gain_t gain = tsl.getGain();
   switch(gain)
   {
     case TSL2591_GAIN_LOW:
-      Serial.println(F("1x (Low)"));
+      //Serial.println(F("1x (Low)"));
       break;
     case TSL2591_GAIN_MED:
-      Serial.println(F("25x (Medium)"));
+      //Serial.println(F("25x (Medium)"));
       break;
     case TSL2591_GAIN_HIGH:
-      Serial.println(F("428x (High)"));
+      //Serial.println(F("428x (High)"));
       break;
     case TSL2591_GAIN_MAX:
-      Serial.println(F("9876x (Max)"));
+      //Serial.println(F("9876x (Max)"));
       break;
   }
-  Serial.print  (F("Timing:       "));
-  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
-  Serial.println(F(" ms"));
-  Serial.println(F("------------------------------------"));
-  Serial.println(F(""));
+//   Serial.print  (F("Timing:       "));
+//   Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+//   Serial.println(F(" ms"));
+//   Serial.println(F("------------------------------------"));
+//   Serial.println(F(""));
 }
